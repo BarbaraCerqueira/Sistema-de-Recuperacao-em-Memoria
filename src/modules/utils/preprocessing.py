7ce839082cabf0b3e2c83d06dc40d36eadd8f330
Bárbara Cerqueira <1barbararodrigues@gmail.com>
2024-04-19 20:51:50 -0300

    Add: Adicionado stemmer

--- src/modules/utils/preprocessing.py
+++ src/modules/utils/preprocessing.py
@@ -1,12 +1,14 @@
 import re
 from unicodedata import normalize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
 
 def normalize_text(text):
     """
-    Preprocess text to remove accents, stopwords, non-alphabetic characters, and convert it to uppercase.
+    Preprocess text to remove accents, stopwords, non-alphabetic characters, apply stemming and convert it to uppercase.
     """
     # Remover acentuação
     text = normalize('NFKD', text).encode('ASCII', 'ignore').decode('utf-8')
@@ -16,7 +18,7 @@ def normalize_text(text):
     text = re.sub(r"[^a-zA-Z]+", " ", text)
     # Remover palavras com apenas 1 letra
     text = re.sub(r'\b\w{1}\b', '', text)
-    # Remover stopwords
+    # Remover stopwords e aplicar stemming
     words = re.findall(r'\b\w+\b', text)
-    filtered_words = [word for word in words if word.lower() not in stop_words]
-    return ' '.join(filtered_words)
+    filtered_stemmed_words = [stemmer.stem(word) for word in words if word.lower() not in stop_words]
+    return ' '.join(filtered_stemmed_words)
